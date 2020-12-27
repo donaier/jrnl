804ec849a5ffdbd4a471ca80e60efc6d1d7a523b
donat baier <donat.baier@gmail.com>
2020-12-27 19:25:50 +0100

    pre npm

---
+++ app/javascript/packs/pwa.js
@@ -0,0 +1,7 @@
+import ProgressiveWebApp from 'pwa-rails';
+//
+// document.addEventListener('turbolinks:load', () => {
+//   const progressiveWebApp = new ProgressiveWebApp();
+// })
+//
+console.log('start?');
